239582a830ded7b8e57e85ed1f4dc5df0f961ab7
maria parma <158210075+Mariaparma@users.noreply.github.com>
2024-11-26 14:38:34 -0300

    add table noticias

---
+++ maria_eduarda_sql.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE portal_db;
+
+CREATE TABLE noticias (
+
+id SERIAL PRIMARY KEY,
+autor VARCHAR(50) NOT NULL,
+titulo VARCHAR (100),
+data_publicacao,
+imagem_relacionada,
+status_publicacao
+);
+
+INSERT INTO noticias () VALUES ()
+
