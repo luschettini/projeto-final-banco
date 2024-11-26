4332eb50c282b84d884ecb436af7f8dbebb2ceb8
Luiza Schettini <luizaschettinii@gmail.com>
2024-11-26 15:57:53 -0300

    feat:

--- luiza_schettini_sql.sql
+++ luiza_schettini_sql.sql
@@ -1,4 +1,4 @@
--- Criação da tabela Carreiras
+-- Criação da tabela de Carreiras  
 CREATE TABLE carreiras(
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(225) NOT NULL, 
@@ -12,5 +12,4 @@ CREATE TABLE carreiras(
 INSERT INTO carreiras (titulo, descricao, mercado, salario, link_cursoSenai, link_faculdadeSenai)
 VALUES
 ('Técnico em Automação Industrial', 'O Técnico em Automação Industrial é responsável por projetar, instalar e manter sistemas automatizados que controlam processos em fábricas e indústrias. Ele programa dispositivos como controladores lógicos programáveis (CLPs) e sensores para coordenar máquinas de produção, garantindo maior eficiência, segurança e redução de custos.', 'Indústrias de alimentos, bebidas, automobilística...', 'R$ 2.500 a R$ 8.000', 'https://www.sp.senai.br/curso/tecnico-em-automacao-industrial/95708', 'https://faculdades.sp.senai.br/curso/77171/automacao-industrial'),
-
 
